7150b0ef82b56f7157051807f19a387d5893cb99
Райымбек <@raiymb>
2023-04-14 15:39:33 +0600

    adding no-argument constructor and changing the constructor

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,10 +1,17 @@
 public class MyArrayList {
     private Object[] realArray;
     private int size;
+    public MyArrayList(){
+        this(5);
+    }
 
     public MyArrayList(int start_size){
-        realArray =new Object[start_size];
+        if(start_size<0){
+            throw new IllegalArgumentException("Size of array can't be negative");
+        }
+        this.realArray =new Object[start_size];
     }
+
 
 
 }
