5ce3d6e57ad4e58faf6e2f9f2afe3d254eeabbb8
Райымбек <@raiymb>
2023-04-14 15:31:53 +0600

    adding interface

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,10 @@
 public class MyArrayList {
+    private Object[] realArray;
+    private int size;
+
+    public MyArrayList(int start_size){
+        realArray =new Object[start_size];
+    }
+
+
 }
