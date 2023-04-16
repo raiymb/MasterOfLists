6a6fdaadf072e2b95bfe641cc6b2f6381f2dae8c
Райымбек <@raiymb>
2023-04-16 14:39:43 +0600

    expanding interface

--- src/MyList.java
+++ src/MyList.java
@@ -1,6 +1,13 @@
 public interface MyList<T> extends Iterable<T> {
     void add(T item);
+    void add(T item, int index);
     T get(int index);
     T remove(int index);
+    boolean remove(T index);
+    void clear();
     int size();
+    boolean contains(Object o);
+    int lastIndexOf(Object o);
+    int indexOf(Object o);
+    void sort();
 }
